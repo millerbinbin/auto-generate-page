08d6d7bec0a7aed428050111854fb739f7d436c6
hubin6 <hubin6@jd.com>
2017-12-05 18:18:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id='app'>
+    <el-container>
+      <el-header style='padding:0; background: #0093D5'>
+        <el-row>
+          <el-col :span="21" :offset="3">
+            <el-menu
+              default-active="1"
+              mode="horizontal"
+              text-color="#fff"
+              background-color="#0093D5"
+              active-text-color="#ffd04b">
+              <el-menu-item index="1">AAA</el-menu-item>
+              <el-menu-item index="2">BBB</el-menu-item>
+              <el-menu-item index="3">CCC</el-menu-item>
+              <el-menu-item index="4">DDD</el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+        
+      </el-header>
+      <el-container>
+        <el-aside width="200px" style='background: #252D2F; height: 100%'>
+          <el-menu
+            default-active="2"
+            background-color="#252D2F"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title"><span>AA1</span></template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">A11</el-menu-item>
+                <el-menu-item index="1-2">A12</el-menu-item>
+                <el-menu-item index="1-3">A13</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><span>BB1</span></template>
+              <el-menu-item-group>
+                <el-menu-item index="3-1">B11</el-menu-item>
+                <el-menu-item index="3-2">B12</el-menu-item>
+                <el-menu-item index="3-3">B13</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><span>CC1</span></template>
+              <el-menu-item-group>
+                <el-menu-item index="3-1">C11</el-menu-item>
+                <el-menu-item index="3-2">C12</el-menu-item>
+                <el-menu-item index="3-3">C13</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+              
+            </el-menu>
+          </el-col>
+        </el-row>       
+        </el-aside>
+        <el-container>
+          <el-main>
+            <v-app>
+              <router-view></router-view>
+            </v-app>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<style>
+
+body {
+  background-color: white;
+  margin: 0;
+}
+.application {
+  padding-top: 0px;
+  background-color: white;
+  color: black;
+  font-size: 15px;
+}
+
+</style>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <el-row :gutter="10" :type="flex">
+      <el-col :span="6" v-for="(col, idx) in columns" :key="col.name" v-if="col.filter">
+        <span style="padding-right: 10px">{{col.label}}:</span>
+        <el-input style="width: 50%"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8" :offset="8">
+        <el-button type="primary" @click="onSearch">查询</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column v-for="(col, idx) in columns" :key="col.name" :prop="col.name" :label="col.label"></el-table-column>
+      </el-table>
+    </el-row>
+  </div>
+</template>
+
+<style>
+  .el-row, .el-col {
+    padding-bottom: 15px;
+  }
+</style>
+<script>
+  export default {
+    data: function () {
+      return {
+        columns: [{
+          name: 'userId',
+          label: '用户id',
+          type: 'string',
+          required: true
+        },
+        {
+          name: 'username',
+          label: '登录账号',
+          type: 'string',
+          filter: true,
+          search: 'complete',
+          required: true
+        },
+        {
+          name: 'cellphone',
+          label: '手机号',
+          type: 'string',
+          filter: true,
+          search: 'fuzzy'
+        },
+        {
+          name: 'roleId',
+          label: '角色',
+          type: 'number',
+          filter: true,
+          selection_list: {
+            type: 'entity',
+            value: '{role.name}|{role.id}'
+          },
+          required: true
+        },
+        {
+          name: 'level',
+          label: '等级',
+          type: 'number',
+          filter: true,
+          selection: true,
+          selection_list: {
+            type: 'list',
+            value: '1,2,3,4'
+          }
+        },
+        {
+          name: 'active',
+          label: '是否激活',
+          type: 'boolean',
+          filter: true,
+          required: true
+        }],
+        tableData: [{
+          userId: 'wxh',
+          username: '王小虎',
+          cellphone: '15000010129',
+          roleId: 3,
+          level: 1,
+          active: 1
+        }, {
+          userId: 'fc11',
+          username: '方尺',
+          cellphone: '18891817376',
+          roleId: 3,
+          level: 1,
+          active: 1
+        }, {
+          userId: 'zs001',
+          username: '周山',
+          cellphone: '13388976373',
+          roleId: 3,
+          level: 1,
+          active: 1
+        }, {
+          userId: 'csr001',
+          username: '测试人员1',
+          cellphone: '15034010129',
+          roleId: 3,
+          level: 1,
+          active: 1
+        }]
+      }
+    }
+  }
+</script>
