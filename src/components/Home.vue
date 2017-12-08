fe8e0e19eed54b1664117d819a4f6c3c4f727c1c
hubin6 <hubin6@jd.com>
2017-12-08 18:02:10 +0800

    refine feature

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,8 @@
           <el-col :span="16">
             <el-input v-if="col.type==='input'" v-model="query[col.name]" style="width: 80%" clearable></el-input>
             <el-select v-else-if="col.type==='select'" v-model="query[col.name]" placeholder="请选择">
-              <el-option v-for="item in col.selection_list" :key="item.key" :label="item.value" :value="item.key"></el-option>
+              <el-option :label="null" :value="null"></el-option>
+              <el-option v-for="item in col.selectionList" :key="item.key" :label="item.value" :value="item.key"></el-option>
             </el-select>
             <el-date-picker v-else-if="col.type==='date'" v-model="query[col.name]" type="date" style="width: 80%" placeholder="选择日期"></el-date-picker>
           </el-col>
@@ -18,13 +19,13 @@
     </el-row>
     <el-row>
       <el-col :span="8" :offset="8" style="text-align: center">
-        <el-button size="medium" type="primary" @click="onSearch">查询</el-button>
+        <el-button size="medium" type="primary" @click="handleSearch">查询</el-button>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="8">
+      <el-col :span="16">
         <el-button size="small" type="info" @click="handleAdd">新建</el-button>
-        <el-button size="small" type="info" >批量删除</el-button>
+        <el-button size="small" type="info" @click="handleBatchDelete">批量删除</el-button>
         <el-button size="small" type="info" >导入</el-button>
         <el-button size="small" type="info" >导出</el-button>
       </el-col>
@@ -32,13 +33,14 @@
       <el-dialog title="新建" :visible.sync="showAddDialog">
         <el-form :rules="rules" :model="addList" ref="addList" :label-position="labelPosition" label-width="30%">
           <el-row>
-            <el-col :span="12" v-for="(item, idx) in row" :key="item.name">
+            <el-col :span="12" v-for="(item, idx) in addRow" :key="item.name">
               <el-form-item :label="item.label + ':'" :prop="item.name">
                 <el-input v-if="item.type==='input'" size="medium" style="width: 80%" v-model="addList[item.name]"></el-input>
                 <el-select v-else-if="item.type==='select'" size="medium" v-model="addList[item.name]">
-                  <el-option v-for="key in Object.keys(item.selectionList)" :key="key" :label="item.selectionList[key]" :value="key"></el-option>
+                  <el-option :label="null" :value="null"></el-option>
+                  <el-option v-for="item in item.selectionList" :key="item.key" :label="item.value" :value="item.key"></el-option>
                 </el-select>
-                <el-date-picker v-else-if="item.type==='date'" v-model="addList[item.name]" type="date" style="width: 80%" placeholder="选择日期"></el-date-picker>
+                <el-date-picker v-else-if="item.type==='date'" v-model="addList[item.name]" type="date" value-format="yyyy-MM-dd" style="width: 80%" placeholder="选择日期"></el-date-picker>
               </el-form-item>
             </el-col>
           </el-row>
@@ -52,13 +54,9 @@
       <el-dialog title="查看" :visible.sync="showOpenDialog">
         <el-form :rules="rules" :label-position="labelPosition" label-width="30%">
           <el-row>
-            <el-col :span="12" v-for="(item, idx) in row" :key="item.name">
+            <el-col :span="12" v-for="(item, idx) in openRow" :key="item.name">
               <el-form-item :label="item.label + ':'" :prop="item.name">
-                <el-input v-if="item.type==='input'" size="medium" disabled style="width: 80%" :value="item.value"></el-input>
-                <el-select v-else-if="item.type==='select'" size="medium" disabled :value="item.value">
-                  <el-option label="" value=""></el-option>
-                </el-select>
-                <el-date-picker v-else-if="item.type==='date'" :value="item.value" type="date" disabled style="width: 80%" placeholder="选择日期"></el-date-picker>
+                <el-input size="medium" disabled style="width: 80%" :value="item.value"></el-input>
               </el-form-item>
             </el-col>
           </el-row>
@@ -68,15 +66,15 @@
       <el-dialog title="修改" :visible.sync="showEditDialog">
         <el-form :rules="rules" :model="editList" ref="editList" :label-position="labelPosition" label-width="30%">
           <el-row>
-            <el-col :span="12" v-for="(item, idx) in row" :key="item.name">
+            <el-col :span="12" v-for="(item, idx) in editRow" :key="item.name">
               <el-form-item :label="item.label + ':'" :prop="item.name">
                 <el-input v-if="item.type==='input'" size="medium" style="width: 80%" v-model="editList[item.name]"></el-input>
                 <el-select v-else-if="item.type==='select'" size="medium" v-model="editList[item.name]" @change="onSelect">
-                  <el-option label="" value=""></el-option>
-                  <el-option v-for="key in Object.keys(item.selectionList)" :key="key" :label="item.selectionList[key]" :value="key"></el-option>
+                  <el-option :label="null" :value="null"></el-option>
+                  <el-option v-for="item in item.selectionList" :key="item.key" :label="item.value" :value="item.key"></el-option>
                 </el-select>
                 <el-date-picker v-else-if="item.type==='date'" v-model="editList[item.name]" 
-                  type="date" style="width: 80%" placeholder="选择日期" @change="onSelect">
+                  type="date" value-format="yyyy-MM-dd" style="width: 80%" placeholder="选择日期" @change="onSelect">
                 </el-date-picker>
               </el-form-item>
             </el-col>
@@ -88,10 +86,15 @@
         </div>
       </el-dialog>
 
-      <el-table :data="tableData" border style="width: 100%" @selection-change="handleSelectionChange" row-key="id">
+      <el-table v-loading="loading" :data="tableData" border style="width: 100%" @selection-change="handleSelectionChange" row-key="id">
         <el-table-column type="selection" width="40"></el-table-column>
-        <el-table-column v-for="(col, idx) in columns" :key="col.name" :prop="col.name" :label="col.label"></el-table-column>
-        <el-table-column label="操作">
+        <el-table-column v-for="(col, idx) in columns" :key="col.name" :prop="col.name" :label="col.label">
+          <template slot-scope="scope" >
+            <span v-if="col.type==='select'">{{ getValueFromKey(scope.row[col.name], kvMapping[col.name]) }}</span>
+            <span v-else>{{ scope.row[col.name] }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" fixed="right">
           <template slot-scope="scope">
             <el-col :span="8">
               <el-button size="mini" type="info" @click="handleOpen(scope.$index, scope.row)">查看</el-button>
@@ -109,7 +112,7 @@
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="1"
-        :page-sizes="[10, 20, 50, 100]"
+        :page-sizes="[10, 20, 40]"
         :page-size="10"
         layout="total, sizes, prev, pager, next"
         :total="this.tableData.length">
@@ -130,9 +133,6 @@
   .el-button--mini, .el-button--mini.is-round {
     padding: 5px 8px
   }
-  .el-button--medium, .el-button--medium.is-round {
-    margin: 0px
-  }
 
   .el-form-item__label {
     font-size: 13px;
@@ -140,6 +140,7 @@
 </style>
 <script>
   import axios from 'axios'
+  import qs from 'qs'
   export default {
     data: function () {
       return {
@@ -152,31 +153,41 @@
         showAddDialog: false,
         showOpenDialog: false,
         showEditDialog: false,
-        curIdx: -1,
-        row: [],
+        curId: -1,
+        addRow: null,
+        openRow: null,
+        editRow: null,
+        null: null,
         kvMapping: {},
         key: null,
         selectedId: [],
         columns: [],
-        tableData: []
+        tableData: [],
+        loading: true
       }
     },
     methods: {
-      onSearch: function () {},
-      ruleCheck: function (key) {
-        console.log(this.rules[key])
+      handleSearch: function () {
+        this.getData(this.query)
       },
       handleDelete: function (index, row) {
-        this.$confirm('确认删除 ' + row[this.key] + ' 的数据?', '提示', {
+        this.$confirm('确认删除这条数据?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          this.tableData.splice(index, 1)
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          })
+          axios.delete('/api/data/t_user/' + row.id)
+            .then(function (response) {
+              console.log(response)
+              this.getData()
+              this.$message({
+                type: 'success',
+                message: '删除成功!'
+              })
+            }.bind(this))
+            .catch(function (error) {
+              console.log(error)
+            })
         }).catch(() => {
           this.$message({
             type: 'info',
@@ -185,72 +196,99 @@
         })
       },
       handleBatchDelete: function (index, row) {
-        this.$confirm('确认批量删除所有数据?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.tableData.splice(index, 1)
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
+        if (this.selectedId.length > 0) {
+          this.$confirm('确认批量删除所有数据?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
+            axios.delete('/api/data/t_user/multiple/' + this.selectedId.join(','))
+              .then(function (response) {
+                console.log(response)
+                this.getData()
+                this.$message({
+                  type: 'success',
+                  message: '删除成功!'
+                })
+              }.bind(this))
+              .catch(function (error) {
+                console.log(error)
+              })
+          }).catch(() => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
           })
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
-        })
+        }
       },
       handleAdd: function () {
-        this.row = []
+        this.addRow = []
         this.addList = {}
         this.columns.forEach(function (item) {
-          this.row.push({name: item.name, label: item.label, type: item.type, selectionList: item.selectionList})
+          this.addRow.push({name: item.name, label: item.label, type: item.type, selectionList: item.selectionList})
         }.bind(this))
         this.showAddDialog = true
       },
       Add: function () {
         this.$refs['addList'].validate((valid) => {
           if (valid) {
-            let tmp = {}
-            for (let k in this.addList) {
-              tmp[k] = getValueFromKey(this.addList[k], this.kvMapping[k])
-            }
-            this.tableData.push(tmp)
+            axios.post('/api/data/t_user', this.addList)
+            .then(function (response) {
+              console.log(response)
+              this.$message({
+                type: 'success',
+                message: '新建成功!'
+              })
+              this.getData()
+            }.bind(this))
+            .catch(function (error) {
+              console.log(error)
+            })
             this.showAddDialog = !this.showAddDialog
+          } else {
+            console.log('error submit')
           }
         })
       },
       handleEdit: function (index, row) {
-        this.curIdx = index
-        this.row = []
+        this.curId = row.id
+        this.editRow = []
         this.editList = {}
         this.columns.forEach(function (item) {
-          this.row.push({name: item.name, label: item.label, type: item.type, selectionList: item.selectionList})
-          this.editList[item.name] = getKeyFromValue(row[item.name], this.kvMapping[item.name])
+          this.editRow.push({name: item.name, label: item.label, type: item.type, selectionList: item.selectionList})
+          this.editList[item.name] = row[item.name]
         }.bind(this))
         this.showEditDialog = true
       },
       Edit: function () {
         this.$refs['editList'].validate((valid) => {
           if (valid) {
-            let tmp = {}
-            for (let k in this.editList) {
-              tmp[k] = getValueFromKey(this.editList[k], this.kvMapping[k])
-            }
-            this.tableData[this.curIdx] = tmp
-            this.tableData = JSON.parse(JSON.stringify(this.tableData))
+            axios.put('/api/data/t_user/' + this.curId, this.editList)
+            .then(function (response) {
+              console.log(response)
+              this.$message({
+                type: 'success',
+                message: '修改成功!'
+              })
+              this.getData()
+            }.bind(this))
+            .catch(function (error) {
+              console.log(error)
+            })
             this.showEditDialog = !this.showEditDialog
+          } else {
+            console.log('error submit')
           }
         })
       },
       handleOpen: function (index, row) {
-        this.row = []
+        this.openRow = []
         this.columns.forEach(function (item) {
-          this.row.push({name: item.name, label: item.label, type: item.type, value: row[item.name]})
+          if (row[item.name]) this.openRow.push({name: item.name, label: item.label, type: item.type, value: this.getValueFromKey(row[item.name], this.kvMapping[item.name])})
+          else this.openRow.push({name: item.name, label: item.label, type: item.type})
         }.bind(this))
-        this.showOpenDialog = true
+        this.showOpenDialog = !this.showOpenDialog
       },
       onSelect: function () {
         this.editList = JSON.parse(JSON.stringify(this.editList))
@@ -273,49 +311,39 @@
             if (item.selectionList) this.kvMapping[item.name] = item.selectionList
             if (item.key) this.key = item.name
           }.bind(this))
+          this.getData()
         }.bind(this))
         .catch(function (error) {
           console.log(error)
         })
       },
-      getData: function () {
-        axios.get('/api/data/t_user')
+      getData: function (condition) {
+        this.loading = true
+        let params = ''
+        if (condition) {
+          params = '?' + qs.stringify(condition)
+        }
+        axios.get('/api/data/t_user' + params)
         .then(function (response) {
           this.tableData = response.data
+          this.loading = !this.loading
         }.bind(this))
         .catch(function (error) {
           console.log(error)
         })
       },
-      rowFormatter: function (row, column) {
-        console.log(row)
+      getValueFromKey: function (key, mapping) {
+        if (mapping) {
+          for (let index = 0; index < mapping.length; index++) {
+            const element = mapping[index]
+            if (key === element.key) return element.value
+          }
+          return null
+        } else return key
       }
     },
     mounted: function () {
       this.getColumns()
-      this.getData()
     }
-  }
-
-  function getKeyFromValue (value, kvMapping) {
-    if (kvMapping) {
-      for (let index = 0; index < kvMapping.length; index++) {
-        const element = kvMapping[index]
-        if (element.value === value) return element.key
-      }
-      return null
-    } else {
-      return value
-    }
-  }
-
-  function getValueFromKey (key, kvMapping) {
-    if (kvMapping) {
-      for (let index = 0; index < kvMapping.length; index++) {
-        const element = kvMapping[index]
-        if (element.key === key) return element.value
-      }
-      return null
-    } else return key
   }
 </script>
